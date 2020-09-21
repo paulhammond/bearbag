2f874b2d336a605167c3f98a3f0ae770af8fd27c
Paul Hammond <paul@paulhammond.org>
2020-09-21 13:45:37 -0700

    Read from the database

--- Package.swift
+++ Package.swift
@@ -6,15 +6,12 @@ import PackageDescription
 let package = Package(
     name: "bearbag",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "bearbag",
-            dependencies: []),
+            dependencies: [.product(name: "SQLite", package: "SQLite.swift") ]),
         .testTarget(
             name: "bearbagTests",
             dependencies: ["bearbag"]),
--- Sources/bearbag/main.swift
+++ Sources/bearbag/main.swift
@@ -1,1 +1,12 @@
-print("Hello, world!")
+import Foundation
+import SQLite
+
+let home = NSHomeDirectory()
+let db = try Connection(
+  "\(home)/Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/Application Data/database.sqlite")
+
+let notes = Table("ZSFNOTE")
+
+let count = try db.scalar(notes.count)
+
+print("db has \(count) notes")
