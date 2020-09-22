9f3b692e7410f778617d92404ba3692dc931abf5
Paul Hammond <paul@paulhammond.org>
2020-09-21 19:09:04 -0700

    Write to directories based on first tag

--- Sources/bearbag/main.swift
+++ Sources/bearbag/main.swift
@@ -28,11 +28,17 @@ let text = Expression<String>("ZTEXT")
 let creation = Expression<Date>("ZCREATIONDATE")
 for note in try db.prepare(notes.select(uuid, title, text).order(creation.desc)) {
 
-  let fileURL = outputDir.appendingPathComponent("\(cleanTitle(note[title])).md")
-  print("title: \(fileURL)")
+  var path = cleanTitle(note[title])
+  let dir = getDir(note[text])
+  if dir != nil {
+    path = "\(dir!)/\(path)"
+  }
+
+  let fileURL = outputDir.appendingPathComponent("\(path).md")
+  print("writing: \(fileURL.path)")
 
   let md = processContent(uuid: note[uuid], contents: note[text])
-
+  try mkdir(fileURL.deletingLastPathComponent())
   try md.write(
     to: fileURL,
     atomically: true,
@@ -49,6 +55,36 @@ func cleanTitle(_ str: String) -> String {
   return String(clean.prefix(100))
 }
 
+func getDir(_ str: String) -> String? {
+  let tags = extractTags(str)
+  return tags[0].components(separatedBy: "/").first
+}
+
+func extractTags(_ str: String) -> [String] {
+  let regex = try! NSRegularExpression(pattern: "#[\\w\\/]+")
+
+  let nsStr = str as NSString
+  return regex.matches(in: str, options: [], range: NSRange(location: 0, length: nsStr.length)).map
+  {
+    String(nsStr.substring(with: $0.range).dropFirst())
+  }.unique()
+}
+
+func mkdir(_ dir: URL) throws {
+  if !FileManager.default.fileExists(atPath: dir.path) {
+    print("creating directory \(dir.path)")
+    try FileManager.default.createDirectory(
+      at: dir, withIntermediateDirectories: true, attributes: nil)
+  }
+}
+
 func processContent(uuid: String, contents: String) -> String {
   return "\(contents)\n\nBearID: \(uuid)\n"
+}
+
+extension Sequence where Iterator.Element: Hashable {
+  func unique() -> [Iterator.Element] {
+    var seen: Set<Iterator.Element> = []
+    return filter { seen.insert($0).inserted }
+  }
 }
